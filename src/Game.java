173d00d5fa3bd72898f31c910174606c59b47bd2
Edward A <dmax7400@gmail.com>
2023-02-02 01:01:06 -0500

    Created Rules/Mechanics, Character Info, and Updated Game

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,7 @@ public class Game {
 
     public static void main(String[] args) 
     {
-        System.out.println("Welcome to Code Combat");
+        System.out.println("******** WELCOME TO CODE COMBAT ********");
 
         
         Scanner sc = new Scanner(System.in);
@@ -21,21 +21,44 @@ public class Game {
 
     }
 
+    /**
+     * Prints the rules and mechnics of the game to the player via the console.
+     * 
+     */
     public static void printRulesMechanics()
     {
         System.out.println("RULES AND MECHNICS: ");
         
+        // Open file and print each line until done.
 
         
     }
 
+    /**
+     * Check if input is valid by seeing if it matches the specified criteria.
+     * 
+     * @param - input
+     * @return - isValid
+     */
     public static boolean userInputValid(String input)
     {   
         boolean isValid = true;
-        // Check if input is valid by seeing if it matches the specified criteria.
+        
+
+        // Repeatedly tell them that their entry is invalid.
+        
+        // Need some way to test all potential input (character names, moves, attack, defend, etc).
+        // If not, I might need methods that check each type of input.
+        // OH! Or I can have a method in each character object that 
 
 
         return isValid;
     }
+
+    // Method to create all of the Characters from the Character Interface.
+    // public static Character ArrayList ()
+    // {
+
+    // } 
 
 }
