6c868833ab7501b9c4a617cc7ce71f2a32c015bf
Edward A <dmax7400@gmail.com>
2023-03-08 18:02:32 -0500

    Added test method for Tank attack method

--- src/Testing/TankTest.java
+++ src/Testing/TankTest.java
@@ -13,8 +13,17 @@ public class TankTest {
     {
         Character chara = new Tank("test");
         chara.takeDamage(150);
-
         assertEquals(2100, chara.getHealth());
+    }
+
+    @Test
+    public void attack_200_ShouldEqual1650()
+    {
+        // Assuming the Opponent's Health is 1500 and Defense is 300...
+        Character chara = new Tank("test");
+        int oppHealth = chara.attack(1500, 300);
+        assertEquals(1600, oppHealth);
+
     }
 
 }
