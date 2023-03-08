8c2d4006e6f59e1d75293b3d5b260e12e6a1f45b
Edward A <dmax7400@gmail.com>
2023-03-08 00:33:18 -0500

    Created takeDamage JUnit Test

--- src/Testing/TankTest.java
+++ src/Testing/TankTest.java
@@ -1,5 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+
 // import static org.junit.
 
-class TankTest {
+public class TankTest {
     
+    @Test
+    public void takeDamage_150_ShouldEqual2100()
+    {
+        Character chara = new Tank("test");
+        chara.takeDamage(150);
+
+        assertEquals(2100, chara.getHealth());
+    }
+
 }
