0d2ab53379e3881b132a60d665d9642e4c851fca
Eddie's Laptop <dmax7400@gmail.com>
2024-05-02 23:59:20 -0400

    created a Player Class

---
+++ src/Player.java
@@ -0,0 +1,14 @@
+public class Player {
+    // Randome bs that will fill out later.
+    String player = "";
+    int move = 0;
+
+    public Player () {
+        // This is a constructor.
+    }
+
+    
+
+    
+    
+}
