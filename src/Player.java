afceae797025c3349a6e2a1d61e7e2b6b693958c
Eddie's Laptop <dmax7400@gmail.com>
2024-05-08 23:47:09 -0400

    Created a method to assign the Player to their character. And a method stub for endTurn.

--- src/Player.java
+++ src/Player.java
@@ -2,12 +2,26 @@ public class Player {
     // Randome bs that will fill out later.
     String player = "";
     int move = 0;
+    Characters playerCharacter = null;
 
-    public Player () {
+    public Player() {
         // This is a constructor.
     }
 
-    
+    // Connect the Player to the Character they are playing as.
+    public void assignCharacter(Characters playerCharacterChoice) {
+        this.playerCharacter = playerCharacterChoice;
+
+    }
+
+    // Functionality to end the player's current turn.
+    public void endTurn() {
+
+    }
+
+    // Maybe I also need some kind of "startTurn" functionality as well?
+
+
 
     
     
